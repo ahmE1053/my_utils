cca7b3c75dcfbe3ad571c5a4fb1ba16186d6a975
Ahmed Shawky <tatabesosmsm@gmail.com>
2025-01-02 08:27:52 +0200

    Refactor cached network image usage and add duration formatting extension

--- lib/src/core/cached_network_image.dart
+++ lib/src/core/cached_network_image.dart
@@ -27,7 +27,6 @@ class CachedNetworkImageWithLoader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CachedNetworkImage(
-      // imageUrl: imageUrl,
       imageUrl: imageUrl,
       alignment: alignment ?? Alignment.center,
       imageBuilder: useInk
--- lib/src/core/duration_extension.dart
+++ lib/src/core/duration_extension.dart
@@ -8,3 +8,12 @@ extension StringDurationExtension on String {
     return Duration(hours: hours, minutes: minutes);
   }
 }
+
+extension DurationExtension on Duration {
+  String get format {
+    final hours = inHours > 0 ? inHours.toString().padLeft(2, '0') : null;
+    final minutes = inMinutes.remainder(60).toString().padLeft(2, '0');
+    final seconds = inSeconds.remainder(60).toString().padLeft(2, '0');
+    return '${hours == null ? '' : '$hours:'}$minutes:$seconds';
+  }
+}
